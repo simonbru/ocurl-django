bd061453328ad9879fc7248a373db91eb6bcd473
Simon Brulhart <simon@brulhart.me>
2019-03-02 22:52:16 +0100

    Give names to URL patterns

--- links/urls.py
+++ links/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 import links.views
 
 urlpatterns = [
-    path('', links.views.hello),
-    path('r/<str:name>/', links.views.shortener_redirect),
+    path('', links.views.hello, name='home'),
+    path('r/<str:name>/', links.views.shortener_redirect, name='link_redirect'),
 ]
