3289af0ea7d57a6cce0b92901b1a8defcb96101a
Simon Brulhart <simon@brulhart.me>
2019-03-02 22:14:14 +0100

    Install django-extensions and django-debug-toolbar

--- ocurl/settings/dev.py
+++ ocurl/settings/dev.py
@@ -1,7 +1,18 @@
-from decouple import config
+from decouple import Csv, config
 
 from .base import *
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 SECRET_KEY = config('SECRET_KEY', default='verysecret')
+
+INTERNAL_IPS = config('INTERNAL_IPS', default='127.0.0.1', cast=Csv())
+
+INSTALLED_APPS += [
+    'django_extensions',
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
--- ocurl/urls.py
+++ ocurl/urls.py
@@ -13,11 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import include, path
 
 urlpatterns = [
     path('app/', include('links.urls')),
     path('app/admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+    except ImportError:
+        pass
+    else:
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
