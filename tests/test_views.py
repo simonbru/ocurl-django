9de4c17e28af67a15ab4b81f91006a4ecca2ca3d
Simon Brulhart <simon@brulhart.me>
2019-03-02 22:24:36 +0100

    Fix existing tests
    * Use model_mommy to create models
    * Remove data loading fixture since "links.json" does not exist

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,21 +1,22 @@
-import pytest
-from django.core.management import call_command
+from model_mommy import mommy
+
+from links.models import Link
 
 
 class TestHelloView:
 
     url = '/app/'
 
-    def test_page_works(self, client):
+    def test_page_works(self, db, client):
         r = client.get(self.url)
         assert r.status_code == 200
 
-    @pytest.mark.django_db
-    def test_redirect_work(self, client):
-        r = client.get('/app/r/test')
-        assert r.status_code == 302
 
-    @pytest.fixture(scope='session')
-    def django_db_setup(django_db_setup, django_db_blocker):
-        with django_db_blocker.unblock():
-            call_command('loaddata', 'links.json')
+class TestRedirectView:
+
+    url = '/app/r/'
+
+    def test_redirect_work(self, db, client):
+        mommy.make(Link, name='test', destination='https://something.invalid')
+        r = client.get(self.url + 'test')
+        assert r.status_code == 302 and r.url == 'https://something.invalid'
